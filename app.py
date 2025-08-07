3d767e2c5d38e99fc0ec66aae1c0837cdec464e0
t191137 <yuki123732110@gmail.com>
2025-08-08 04:49:56 +0800

    修正寫法

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
+import os
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, LocationMessage
+
+from linebot.v3.messaging import (
+    MessagingApi, 
+    ApiClient, 
+    Configuration, 
+    ReplyMessageRequest, 
+    TextMessage
+)
+from linebot.v3.webhook import WebhookHandler
+from linebot.v3.webhooks import MessageEvent, TextMessageContent, LocationMessageContent
 
 app = Flask(__name__)
 
-# 這裡要換成你自己的 Channel Access Token 和 Channel Secret
-LINE_CHANNEL_ACCESS_TOKEN = "你的Channel Access Token"
-LINE_CHANNEL_SECRET = "3f96df993ada56c0ee57de6b4434b377"
+LINE_CHANNEL_ACCESS_TOKEN = ""
+LINE_CHANNEL_SECRET = ""
 
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+# "Configuration -> ApiClient -> MessagingApi" 初始化流程
+configuration = Configuration(access_token=LINE_CHANNEL_ACCESS_TOKEN)
+api_client = ApiClient(configuration) 
+messaging_api = MessagingApi(api_client) 
+
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
@@ -22,27 +34,32 @@ def callback():
         abort(400)
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessageContent)
 def handle_text_message(event):
-    if event.message.text == "查詢":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="請傳送您的現在位置給我，我會幫你查附近停車場！")
+    if event.message.text == "check":
+        messaging_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text="請傳送您的現在位置給我，我會幫你查附近停車場！")]
+            )
         )
     else:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="歡迎使用停車查查！輸入『查詢』開始")
+        messaging_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text="歡迎使用停車小狐狸！輸入『check』開始")] # 我稍微改了提示文字，讓使用者知道要輸入 check
+            )
         )
 
-@handler.add(MessageEvent, message=LocationMessage)
+@handler.add(MessageEvent, message=LocationMessageContent)
 def handle_location_message(event):
     latitude = event.message.latitude
     longitude = event.message.longitude
-    # 這裡未來會換成查停車場
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=f"你的位置座標：{latitude}, {longitude}。\n未來我會幫你查附近停車場！")
+    messaging_api.reply_message(
+        ReplyMessageRequest(
+            reply_token=event.reply_token,
+            messages=[TextMessage(text=f"你的位置座標：{latitude}, {longitude}。\n未來我會幫你查附近停車場！")]
+        )
     )
 
 if __name__ == "__main__":
